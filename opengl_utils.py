ded470d00bb7698e31c3b0723eb8385870787fed
srij1234 <srij123451@gmail.com>
2024-11-01 11:25:09 +0530

    modified:   __pycache__/opengl_utils.cpython-311.pyc 	modified:   opengl_utils.py

--- opengl_utils.py
+++ opengl_utils.py
@@ -9,13 +9,14 @@ def init_opengl():
     glEnable(GL_NORMALIZE)  # Normalize normals for lighting
 
 def init_lighting():
-    glEnable(GL_LIGHTING)
-    glEnable(GL_LIGHT0)
-    light_pos = [0.0, 0.0, 2.0, 1.0]
-    glLightfv(GL_LIGHT0, GL_POSITION, light_pos)
-    glLightfv(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
-    glEnable(GL_COLOR_MATERIAL)
-    glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
+    glDisable(GL_LIGHTING)
+    # glEnable(GL_LIGHTING)
+    # glEnable(GL_LIGHT0)
+    # light_pos = [0.0, 0.0, 2.0, 1.0]
+    # glLightfv(GL_LIGHT0, GL_POSITION, light_pos)
+    # glLightfv(GL_LIGHT0, GL_DIFFUSE, [1, 1, 1, 1])
+    # glEnable(GL_COLOR_MATERIAL)
+    # glColorMaterial(GL_FRONT_AND_BACK, GL_AMBIENT_AND_DIFFUSE)
 
 def apply_rotation_matrix(matrix):
     rotation = np.identity(4, dtype=np.float32)
